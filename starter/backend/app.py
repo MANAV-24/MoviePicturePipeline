481c370b1fe50f8080da93630662c0b88fcc25b3
MANAV_24 <manavnandam@gmailcom>
2026-09-13 17:57:44 +0000

    Enables CORS on backend to allow cross-origin frontend requests

--- starter/backend/app.py
+++ starter/backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 from movies import catalogue
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     @app.get("/health")
     def health():
